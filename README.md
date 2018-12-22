6c555c01cecaedb3fc9d2e4a867b6684e6a85c74
Eduardo Marques <33849830+dukmarques@users.noreply.github.com>
2018-12-22 04:06:50 -0300

    add Slider

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Organizze Clone
+
+
+#Biblioteca utilizada para o Slider de apresentação do App:
+https://github.com/heinrichreimer/material-intro
--- app/src/main/java/com/example/eduardo/organizze/MainActivity.java
+++ app/src/main/java/com/example/eduardo/organizze/MainActivity.java
@@ -3,11 +3,41 @@ package com.example.eduardo.organizze;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+
+public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main); //Remover isto, pois não é mais necessário
+
+        //Remoção dos buttões de navegação
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        //Add sliders
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_1)
+                .build()
+        );
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_2)
+                .build()
+        );
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_3)
+                .build()
+        );
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_4)
+                .build()
+        );
+
     }
 }
