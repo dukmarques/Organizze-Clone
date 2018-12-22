af62bfbc8615195b723b965dac2ef3fc09bc6188
dukmarques <eduardomarques@outlook.com>
2018-12-22 16:38:51 -0300

    registration interface and login

--- app/src/main/java/com/example/eduardo/organizze/MainActivity.java
+++ app/src/main/java/com/example/eduardo/organizze/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.eduardo.organizze;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.eduardo.organizze.activity.CadastroActivity;
+import com.example.eduardo.organizze.activity.LoginActivity;
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 
@@ -36,9 +40,22 @@ public class MainActivity extends IntroActivity {
         addSlide(new FragmentSlide.Builder()
                 .background(android.R.color.white)
                 .fragment(R.layout.intro_4)
+                .build()
+        );
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_cadastro)
+                .canGoBackward(false)
                 .canGoForward(false) //Resolver problema de fechar app ao passar todas as telas
                 .build()
         );
+    }
 
+    public void btEntrar(View v){
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    public void btCadastrar(View v){
+        startActivity(new Intent(this, CadastroActivity.class));
     }
 }
