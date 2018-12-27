9756d2e005b34c82197ba803a7103fdf26b7ae91
Eduardo Marques <33849830+dukmarques@users.noreply.github.com>
2018-12-27 13:35:45 -0300

    Configuring RecyclerView

--- app/src/main/java/com/example/eduardo/organizze/activity/PrincipalActivity.java
+++ app/src/main/java/com/example/eduardo/organizze/activity/PrincipalActivity.java
@@ -6,6 +6,8 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,8 +15,10 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.example.eduardo.organizze.R;
+import com.example.eduardo.organizze.adpater.AdapterMovimentacao;
 import com.example.eduardo.organizze.config.ConfiguracaoFirebase;
 import com.example.eduardo.organizze.helper.Base64Custom;
+import com.example.eduardo.organizze.model.Movimentacao;
 import com.example.eduardo.organizze.model.Usuario;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -26,6 +30,8 @@ import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PrincipalActivity extends AppCompatActivity {
     private MaterialCalendarView calendarView;
@@ -39,6 +45,10 @@ public class PrincipalActivity extends AppCompatActivity {
     private DatabaseReference usuarioRef;
     private ValueEventListener valueEventListenerUsuario;
 
+    private RecyclerView recyclerView;
+    private AdapterMovimentacao adapterMovimentacao;
+    private List<Movimentacao> movimentacoes = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,9 +59,20 @@ public class PrincipalActivity extends AppCompatActivity {
 
         textoSaldo = findViewById(R.id.textSaldo);
         textoSaudacao = findViewById(R.id.textSaudacao);
-
         calendarView = findViewById(R.id.calendarView);
+        recyclerView = findViewById(R.id.recyclerMovimentos);
+
         configuraCalendarView();
+
+        //Configurar adapter
+        adapterMovimentacao = new AdapterMovimentacao(movimentacoes, this);
+
+        //Configurar RecyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapterMovimentacao);
+
     }
 
     public void recuperarResumo(){
