4971c82308732bde6ceed8ab30d73e0cf9da7803
Eduardo Marques <33849830+dukmarques@users.noreply.github.com>
2018-12-27 02:25:24 -0300

    Saving Expense

--- app/src/main/java/com/example/eduardo/organizze/activity/DespesasActivity.java
+++ app/src/main/java/com/example/eduardo/organizze/activity/DespesasActivity.java
@@ -3,14 +3,18 @@ package com.example.eduardo.organizze.activity;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 import com.example.eduardo.organizze.R;
 import com.example.eduardo.organizze.helper.DateCustom;
+import com.example.eduardo.organizze.model.Movimentacao;
 
 public class DespesasActivity extends AppCompatActivity {
     private TextInputEditText campoData, campoCategoria, campoDescricao;
     private EditText campoValor;
+    private Movimentacao movimentacao;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +27,16 @@ public class DespesasActivity extends AppCompatActivity {
 
         //Preenche o campo data com a data atual
         campoData.setText(DateCustom.dataAtual());
+    }
+
+    public void salvarDespesas(View view){
+        movimentacao = new Movimentacao();
+        movimentacao.setValor(Double.parseDouble(campoValor.getText().toString()));
+        movimentacao.setCategoria(campoCategoria.getText().toString());
+        movimentacao.setDescricao(campoDescricao.getText().toString());
+        movimentacao.setData(campoData.getText().toString());
+        movimentacao.setTipo("d");
+
+        movimentacao.salvar(campoData.getText().toString());
     }
 }
